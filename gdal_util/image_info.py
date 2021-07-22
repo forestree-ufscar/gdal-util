76f3e80a3151cb25ff9629c4a29f44a62c0ffdc7
Rodolfo Cugler <rodolfocugler@outlook.com>
2021-07-22 07:52:14 -0300

    Read MTL from content

--- gdal_util/image_info.py
+++ gdal_util/image_info.py
@@ -3,22 +3,31 @@ def get_mtl_filename_from_band_file(filename):
     return filename.split("_")[0] + "_MTL.txt"
 
 
+def read_mtl_from_content(content: str):
+    return _read_mtl_from_content(content.splitlines())
+
+
 def read_mtl_file(filepath):
+    with open(filepath) as f:
+        lines = f.readlines()
+    return _read_mtl_from_content(lines)
+
+
+def _read_mtl_from_content(content):
     mtl_content = {}
     current_dict_reference = mtl_content
     previous_dict_reference = mtl_content
-    with open(filepath) as f:
-        for line in f.readlines():
-            stripped_line = line.strip()
-            if stripped_line.startswith("GROUP"):
-                previous_dict_reference = current_dict_reference
-                current_dict_reference[stripped_line[8:]] = {}
-                current_dict_reference = current_dict_reference[stripped_line[8:]]
-            elif stripped_line.startswith("END"):
-                current_dict_reference = previous_dict_reference
-            else:
-                key_value_pair = stripped_line.split(" = ")
-                current_dict_reference[key_value_pair[0]] = key_value_pair[1].replace("\"", "")
+    for line in content:
+        stripped_line = line.strip()
+        if stripped_line.startswith("GROUP"):
+            previous_dict_reference = current_dict_reference
+            current_dict_reference[stripped_line[8:]] = {}
+            current_dict_reference = current_dict_reference[stripped_line[8:]]
+        elif stripped_line.startswith("END"):
+            current_dict_reference = previous_dict_reference
+        else:
+            key_value_pair = stripped_line.split(" = ")
+            current_dict_reference[key_value_pair[0]] = key_value_pair[1].replace("\"", "")
     return mtl_content
 
 
